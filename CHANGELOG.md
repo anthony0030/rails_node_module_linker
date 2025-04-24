e82cfa0f76d32deb879020a6d570fe93ee8016ae
Anthony Veaudry <anthony@veaudry.pro>
2025-04-24 18:39:24 +0300

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [0.1.0] - 2025-04-24
+
+- Initial release
+- Links selected node_modules to `public/` for use with Propshaft
---
+++ lib/rails_node_module_linker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_node_module_linker/version"
+
+module RailsNodeModuleLinker
+  class Error < StandardError; end
+  # Your code goes here...
+end
