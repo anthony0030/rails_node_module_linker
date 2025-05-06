24a0ba894fea3b7e042a3e53b65a4958d15912e5
Anthony Veaudry <anthony@veaudry.pro>
2025-05-06 11:23:06 +0300

    Fix rail tie

--- lib/rails_node_module_linker/railtie.rb
+++ lib/rails_node_module_linker/railtie.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
-require "rails/railtie"
+require 'rails_node_module_linker'
+require 'rails'
 
-module RailsNodeModuleLinker
-  class Railtie < ::Rails::Railtie
+module MyGem
+  class Railtie < Rails::Railtie
+    railtie_name :rails_node_module_linker
+
     rake_tasks do
-      load "rails_node_module_linker/tasks/link.rake"
+      path = File.expand_path(__dir__)
+      Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
     end
   end
 end
