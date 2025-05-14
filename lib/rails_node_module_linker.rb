0333996ae58179e54b5c778eb7bc0db83022fed8
Anthony Veaudry <anthony@veaudry.pro>
2025-05-14 12:41:21 +0300

    Ensure config exists

--- lib/rails_node_module_linker.rb
+++ lib/rails_node_module_linker.rb
@@ -9,6 +9,8 @@ module RailsNodeModuleLinker
   # * Configuration accessor for users to access and modify settings
   mattr_accessor :config
 
+  self.config ||= Config.new
+
   class Engine < ::Rails::Engine
     # * Initialize default configuration
     initializer 'rails_node_module_linker.config' do
